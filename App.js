18b7e65fbc8b54692f112ea45243ded488ebe8a0
@Semenovamm <semenovamm@lad24.ru>
2021-03-12 10:21:30 +0300

    first

--- App.js
+++ App.js
@@ -1,21 +1,184 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  ActivityIndicator,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import * as tf from "@tensorflow/tfjs";
+import { decodeJpeg } from "@tensorflow/tfjs-react-native";
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import Constants from "expo-constants";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import moment from "moment";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  state = {
+    isTfReady: false,
+    isModelReady: false,
+    predictions: null,
+    image: null,
+  };
+
+  getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!");
+      }
+    }
+  };
+
+  classifyImage = async () => {
+    try {
+      const imageAssetPath = Image.resolveAssetSource(this.state.image);
+      const response = await FileSystem.readAsStringAsync(imageAssetPath.uri, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+      const rawImageData = tf.util.encodeString(response, "base64").buffer;
+      const raw = new Uint8Array(rawImageData);
+      const imageTensor = decodeJpeg(raw);
+      const predictions = await this.model.classify(imageTensor);
+      this.setState({ predictions: predictions });
+    } catch (error) {
+      console.log("classifyImage error", error);
+    }
+  };
+
+  selectImage = async () => {
+    try {
+      let response = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+      });
+
+      if (!response.cancelled) {
+        const source = { uri: response.uri };
+        this.setState({ image: source });
+        this.classifyImage();
+      }
+    } catch (error) {
+      console.log("selectImage error", error);
+    }
+  };
+
+  async componentDidMount() {
+    await tf.ready();
+    this.setState({
+      isTfReady: true,
+    });
+    this.model = await mobilenet.load();
+    this.setState({ isModelReady: true });
+    this.getPermissionAsync();
+  }
+
+  renderPrediction = (prediction) => {
+    return (
+      <Text key={prediction.className} style={styles.text}>
+        {prediction.className}
+      </Text>
+    );
+  };
+
+  render() {
+    const { isTfReady, isModelReady, predictions, image } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <View style={styles.loadingContainer}>
+          <Text style={styles.text}>
+            TFJS ready? {isTfReady ? <Text>✅</Text> : ""}
+          </Text>
+          <View style={styles.loadingModelContainer}>
+            <Text style={styles.text}>Model ready? </Text>
+            {isModelReady ? (
+              <Text style={styles.text}>✅</Text>
+            ) : (
+              <ActivityIndicator size="small" />
+            )}
+          </View>
+        </View>
+        <TouchableOpacity
+          style={styles.imageWrapper}
+          onPress={isModelReady ? this.selectImage : undefined}
+        >
+          {image && <Image source={image} style={styles.imageContainer} />}
+          {isModelReady && !image && (
+            <Text style={styles.transparentText}>Tap to choose image</Text>
+          )}
+        </TouchableOpacity>
+        <View style={styles.predictionWrapper}>
+          {isModelReady && image && (
+            <Text style={styles.text}>
+              Predictions: {predictions ? "" : "Predicting..."}
+            </Text>
+          )}
+          {isModelReady &&
+            predictions &&
+            predictions.map((p) => this.renderPrediction(p))}
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#171f24",
+    alignItems: "center",
+  },
+  loadingContainer: {
+    marginTop: 80,
+    justifyContent: "center",
+  },
+  text: {
+    color: "#ffffff",
+    fontSize: 16,
+  },
+  loadingModelContainer: {
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  imageWrapper: {
+    width: 280,
+    height: 280,
+    padding: 10,
+    borderColor: "#cf667f",
+    borderWidth: 5,
+    borderStyle: "dashed",
+    marginTop: 40,
+    marginBottom: 10,
+    position: "relative",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 250,
+    height: 250,
+    position: "absolute",
+    top: 10,
+    left: 10,
+    bottom: 10,
+    right: 10,
+  },
+  predictionWrapper: {
+    height: 100,
+    width: "100%",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  transparentText: {
+    color: "#ffffff",
+    opacity: 0.7,
   },
 });
+
+export default App;
